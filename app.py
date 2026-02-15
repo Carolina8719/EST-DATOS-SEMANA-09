4cfc843569591cdc3ddc0228d15ee2b08132753a
MELANIE CAROLINA VINCES PATIN <mc.vincesp@uea.edu.ec>
2026-02-15 16:52:33 -0500

    Refactor Flask app to change route responses

--- app.py
+++ app.py
@@ -2,13 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def inicio():
-    return '<h1>🏥 Bienvenido al Sistema de Citas Médicas – Clínica Salud+</h1>'
+@app.route("/")
+def home():
+    return "App funcionando en Render 🚀"
 
-@app.route('/cita/<paciente>')
-def cita(paciente):
-    return f'<h2>👤 Hola, {paciente}. Tu cita médica está registrada con éxito.</h2>'
 
-if __name__ == "__main__":
-    app.run(debug=True)
