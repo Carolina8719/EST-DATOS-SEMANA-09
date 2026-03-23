76448110bb0279e6cf8451a7e5919c20d85905e1
Carolina8719 <mc.vincesp@uea.edu.ec>
2026-03-22 21:24:07 -0500

    Semana 14: Script SQL autenticación

---
+++ script_semana14.sql
@@ -0,0 +1,8 @@
+-- Semana 14: Autenticación de usuarios
+-- Sistema de Citas Médicas - Carolina8719
+
+USE citas_medicas;
+
+DESCRIBE usuarios;
+
+SELECT id_usuario, nombre, mail, rol, created_at FROM usuarios;
